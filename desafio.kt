b3f2cc0b9f6b2a5840807a8fe371e6d58798c748
Diogo de Souza Corrêa <136863883+diogosc03@users.noreply.github.com>
2025-11-30 03:06:51 -0300

    Update code for tests

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,102 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {BASICO, INTERMEDIARIO, AVANCADO}
 
-class Usuario
+data class Usuario(val nome: String, val idade: Int, val escolaridade: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+    val inscritos = mutableListOf<Usuario>();
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        for (usuario in usuarios) {
+            inscritos.add(usuario)
+        }
     }
 }
 
+val formacoes = listOf<Formacao>(
+    Formacao(
+        nome = "Front-end Developer",
+        conteudos = listOf(
+            ConteudoEducacional(
+                nome = "HTML5",
+            	duracao = 5,
+            	nivel = Nivel.BASICO
+            ),
+            ConteudoEducacional(
+                nome = "CSS3",
+            	duracao = 5,
+            	nivel = Nivel.BASICO
+            )
+        ),
+        nivel = Nivel.BASICO
+    ),
+    Formacao(
+        nome = "Back-end Developer",
+        conteudos = listOf(
+            ConteudoEducacional(
+                nome = "PHP",
+            	duracao = 5,
+            	nivel = Nivel.BASICO
+            ),
+            ConteudoEducacional(
+                nome = "SQL",
+            	duracao = 5,
+            	nivel = Nivel.BASICO
+            )
+        ),
+        nivel = Nivel.BASICO
+    )
+)
+
+val usuarios = listOf<Usuario>(
+    Usuario(
+        nome = "João Barbosa",
+        idade = 16,
+        escolaridade = "Ensino fundamental completo"
+    ),
+    Usuario(
+        nome = "Ana Maria",
+        idade = 24,
+        escolaridade = "Ensino superior"
+    )
+)
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val actions = listOf(2, 3);
+    var count = 1;
+    
+    for(action in actions) {
+        println("-----------------------------------------")
+        println("----- Bem Vindo às Formações da DIO -----")
+        println("-----------------------------------------")
+        println("1 - Verificar formações disponíveis.")
+        println("2 - Se inscrever em uma formação.")
+        println("3 - Sair")
+        
+        println("\nEscolha uma das opções acima: $action")
+        
+        when(action) {
+            1 -> when(formacoes.size) {
+                0 -> println("\nNenhuma formação disponível.\n")
+                else -> for(formacao in formacoes) {
+                    println("\n************ Formação $count ************")
+                    println("\t${formacao.nome}")
+                    println("\tMódulos: ${formacao.conteudos.size}")
+                    println("\tNível: ${formacao.nivel}")
+
+                    println("\n++++++++++++ Conteudos ++++++++++++++")
+                    for (conteudo in formacao.conteudos) {
+                        println("\t${conteudo.nome} - ${conteudo.duracao}hrs - ${conteudo.nivel}")
+                    }
+
+                    println("\n************************************")
+                    count++
+                }
+            }
+            3 -> println("\nSAINDO...")
+        }
+    }
 }
