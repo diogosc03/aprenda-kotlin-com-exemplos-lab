f2d2a133859d3a5c0314f62b010aa7c190df76ad
Diogo de Souza Corrêa <136863883+diogosc03@users.noreply.github.com>
2025-11-30 04:09:54 -0300

    Refactor matricular method and update main logic

--- desafio.kt
+++ desafio.kt
@@ -9,7 +9,7 @@ data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Ni
 data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
     val inscritos = mutableListOf<Usuario>();
     
-    fun matricular(vararg usuarios: Usuario) {
+    fun matricular(usuarios: List<Usuario>) {
         for (usuario in usuarios) {
             inscritos.add(usuario)
         }
@@ -65,22 +65,24 @@ val usuarios = listOf<Usuario>(
 )
 
 fun main() {
-    val actions = listOf(2, 3);
-    var count = 1;
+    val actions = listOf(2, 1, 3);
+    val formacaoEscolhida = "Front-end Developer"
     
     for(action in actions) {
+        var count = 1;
+        
         println("-----------------------------------------")
         println("----- Bem Vindo às Formações da DIO -----")
         println("-----------------------------------------")
         println("1 - Verificar formações disponíveis.")
-        println("2 - Se inscrever em uma formação.")
+        println("2 - Inscrever alunos em uma formação.")
         println("3 - Sair")
         
         println("\nEscolha uma das opções acima: $action")
         
         when(action) {
             1 -> when(formacoes.size) {
-                0 -> println("\nNenhuma formação disponível.\n")
+                0 -> println("\nNenhuma formação disponível!\n")
                 else -> for(formacao in formacoes) {
                     println("\n************ Formação $count ************")
                     println("\t${formacao.nome}")
@@ -91,12 +93,32 @@ fun main() {
                     for (conteudo in formacao.conteudos) {
                         println("\t${conteudo.nome} - ${conteudo.duracao}hrs - ${conteudo.nivel}")
                     }
+                    
+                    println("\n+++++++++++++ Alunos +++++++++++++++")
+                    if(formacao.inscritos.size > 0) {
+                        for (aluno in formacao.inscritos) {
+                            println("${aluno.nome} | ${aluno.idade} anos - ${aluno.escolaridade}\n")
+                        }
+                    } else {
+                        println("\nNenhuma inscrição ativa!\n")
+                    }
 
-                    println("\n************************************")
+                    println("************************************")
                     count++
                 }
             }
+            2 -> for (formacao in formacoes) {
+                if (formacao.nome == formacaoEscolhida) {
+                    formacao.matricular(usuarios);
+                    println("\nInscrição realizada com sucesso!\n")
+                    break;
+                } else if(count == formacoes.size) {
+                    println("Formação inexistente!")
+                }
+                count++
+            }
             3 -> println("\nSAINDO...")
+            else -> println("\nOpção inválida!\n")
         }
     }
 }
